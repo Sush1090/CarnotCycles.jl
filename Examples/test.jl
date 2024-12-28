ae9ac73357c0ee7f34e2f1eeb12507c857f0feec
Sush1090 <sushrut1090@gmail.com>
2024-12-28 12:42:39 +0100

    tested compressor, evaporator in mixture and pure component clapyeyron #13

--- Examples/test.jl
+++ Examples/test.jl
@@ -1,23 +1,27 @@
 using CarnotCycles, ModelingToolkit, DifferentialEquations, Clapeyron
 
 @independent_variables t
-model = cPR(["ethane"],idealmodel = ReidIdeal);
+model = cPR(["isopentane","toluene"],idealmodel = ReidIdeal);
 load_fluid(model)
 
 @named src = MassSource()
 @named comp = IsentropicCompressor()
+@named evap = SimpleEvaporator()
 @named sink = MassSink()
 eqs = [
     connect(src.port,comp.inport)
-    connect(comp.outport,sink.port)
+    connect(comp.outport,evap.inport)
+    connect(evap.outport,sink.port)
 ]
 
-system=[src,comp,sink]
+system=[src,comp,evap,sink]
 @named cycle = ODESystem(eqs,t,systems= system)
 sys = structural_simplify(cycle)
-para = [sys.src.source_x=>0.5,sys.comp.πc => 2,sys.src.source_mdot => 20]
-# para = [sys.src.source_pressure => 101325,sys.src.source_enthalpy => 1e5,
-#         sys.comp.πc => 3,sys.comp.η=>0.8, sys.src.source_mdot => 0.02]
+# para = [sys.src.source_x=>1.0,sys.comp.πc => 2,sys.src.source_mdot => 20]
+z_ = CarnotCycles.mass_to_moles(model,[0.6,0.4],100)
+h_ = enthalpy(model,101325,300,z_)
+para = [sys.src.source_pressure => 101325,sys.src.source_enthalpy => h_, sys.src.source_x => 0.6,
+        sys.evap.ΔT_sh => 3, sys.src.source_mdot => 100,sys.comp.πc => 5]
 u0 = []
 
 prob = SteadyStateProblem(sys,u0,para)
