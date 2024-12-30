80a4e9ed9f4665e0cad386ddf341ede7e6c49630
Sush1090 <sushrut1090@gmail.com>
2024-12-30 13:36:37 +0100

    added Clapeyron expander

--- src/Components/expander.jl
+++ src/Components/expander.jl
@@ -10,6 +10,7 @@
         throw(error("Fluid not selected"))
     end
 end
+export IsentropicExpander
 
 @component function IsentropicExpanderCoolProp(;name,fluid=set_fluid)
     @named inport = CoolantPort()
@@ -50,6 +51,64 @@ end
    compose(ODESystem(eqs, t, vars, para;name), inport, outport)
 end
 
+function IsentropicExpanderClapeyron(;name,fluid = set_fluid) 
+    @named inport = CoolantPort()
+    @named outport = CoolantPort()
+    @named power =  PowerPort()
+    para = @parameters begin
+        η=0.8, [description = "Isentropic Effeciency",bounds = (0.0,1.0)]
+        πc, [description = "Pressure ratio"]
+    end
+    vars = @variables begin
+        P(t)
+        s_in(t)
+        p_in(t)
+        T_in(t)
+        h_in(t)
+        ρ_in(t)
+        z_in(t)
+        mdot_in(t)
+        x_in(t)
+
+        s_out(t)
+        p_out(t)
+        T_out(t)
+        h_out(t)
+        ρ_out(t)
+        z_out(t)
+        mdot_out(t)
+        x_out(t)
+     end
+   eqs = [  
+            mdot_in ~ inport.mdot
+            x_in ~ inport.x
+            z_in ~ mass_to_moles(fluid,[x_in,1-x_in],mdot_in)
+            p_in ~ inport.p
+            h_in ~ inport.h
+            s_in ~ ph_entropy(fluid,p_in,h_in,z_in)
+            T_in ~ ph_temperature(fluid,p_in,h_in,z_in)
+            ρ_in ~ ph_mass_density(fluid,p_in,h_in,z_in)
+
+            h_out ~ IsentropicExpansionClapeyron(πc,h_in,p_in,z_in,fluid,η)
+            p_out ~ p_in/πc
+            s_out ~ ph_entropy(fluid,p_out,h_out,z_out)
+            z_out ~ z_in
+            T_out ~ ph_temperature(fluid,p_out,h_out,z_out)
+            ρ_out ~ ph_mass_density(fluid,p_out,h_out,z_out)
+            mdot_out ~ mdot_in
+            x_out ~ x_in   
+
+            P ~ h_out - h_in
+
+            outport.p ~ p_out
+            outport.h ~ h_out
+            outport.x ~ x_out
+            outport.mdot ~ mdot_out
+
+            power.P ~ P
+   ]
+   compose(ODESystem(eqs, t, vars, para;name), inport, outport,power)
+end
 
 
 #"""
