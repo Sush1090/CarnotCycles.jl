55a56a1fe370281e9d9c1370958a6c1ebcf0ad4c
Sush1090 <sushrut1090@gmail.com>
2025-03-31 16:47:41 +0200

    added glide of HEX

--- src/CarnotCycles.jl
+++ src/CarnotCycles.jl
@@ -2,18 +2,30 @@ module CarnotCycles
 
 # Write your package code here.
 
-using ModelingToolkit, CoolProp, DifferentialEquations, Plots
+using ModelingToolkit, CoolProp, DifferentialEquations, Plots, Roots
 using Clapeyron
 import Clapeyron.PH
 import Clapeyron.PS
 import ModelingToolkit:scalarize
 
+# """
+# Symbolic Registrations
+# """
+
 include("ClapeyronRegistrations.jl")
+
+# """
+# Utilities
+# """
 
 include("connectors.jl")
 include("Utils.jl")
 include("Processes.jl")
 include("ThemodynamicClaculations.jl")
+
+# """
+# Basic Components
+# """
 
 include("Components/compressor.jl")
 include("Components/expander.jl")
@@ -24,7 +36,22 @@ include("Components/Evaporator.jl")
 include("Components/Condensor.jl")
 include("Components/pump.jl")
 
+# """
+# Simple HEX models
+# """
+
+include("Components/HeatExchangers/SimpleGlide.jl")
+
+# """
+# Storages
+# """
+
 include("Components/ThermalStorages/packedbed.jl")
+
+
+# """
+# Plotting
+# """
 
 include("Plotting/PhasePlot.jl")
 end
