d34683b22fa2161cac781fd5e590599f08a0f086
Sush1090 <sushrut1090@gmail.com>
2025-04-01 15:13:58 +0200

    getting dev page

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,10 @@ push!(LOAD_PATH,"../src/")
 using Documenter,CarnotCycles
 makedocs(sitename = "CarnotCycles.jl",
     modules  = [CarnotCycles],
+    format = Documenter.HTML(
+    # Use clean URLs, unless built as a "local" build
+    canonical = "https://sush1090.github.io/CarnotCycles.jl/dev",
+),
     pages=[
                 "Home" => "index.md"
                ])
