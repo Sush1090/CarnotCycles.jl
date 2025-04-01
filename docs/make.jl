e0a9e5330c50e5d556991d86e53c14b9da506b35
Sush1090 <sushrut1090@gmail.com>
2025-04-01 13:42:08 +0200

    adding docs

---
+++ docs/make.jl
@@ -0,0 +1,13 @@
+push!(LOAD_PATH,"../src/")
+using Documenter,CarnotCycles
+makedocs(sitename = "CarnotCycles.jl",
+    modules  = [CarnotCycles],
+    pages=[
+                "Home" => "index.md"
+               ])
+
+
+deploydocs(;
+               repo="github.com/Sush1090/CarnotCycles.jl",
+           )
+           
