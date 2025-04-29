4614d82bdf3245315e87ad297ae6d3a84714d396
Sush1090 <sushrut1090@gmail.com>
2025-04-30 01:29:50 +0200

    added robust plots

--- src/Plotting/PhasePlot.jl
+++ src/Plotting/PhasePlot.jl
@@ -1,248 +1,204 @@
-
-
-# abstract type PhasePlotType end
-
-# struct PhasePlotType_PH <: PhasePlotType end
-# struct PhasePlotType_TS <: PhasePlotType end
-
-# export PhasePlotType_PH,PhasePlotType_TS
-
-# T_line(p,h,fluid) = PropsSI("T","P",p,"H",h,fluid)
-# S_line(p,h,fluid) = PropsSI("S","P",p,"H",h,fluid)
-# ρ_line(p,h,fluid) = PropsSI("D","P",p,"H",h,fluid)
-
-# function LineData(f::Function,x1,y1,x2,y2,fluid::AbstractString=set_fluid;points = 100)
-#     if isnothing(fluid)
-#         throw(error("Fluid not selected"))
-#     end
-#     x = collect(range(x1,x2,points))
-#     y = collect(range(y1,y2,points))
-#     data = f.(x,y,fluid)
-#     return x,y,data
-# end
-
-
-# function CollectPhaseData(sol::ODESolution,system::Vector{ODESystem},fluid::AbstractString=set_fluid;points = 100,p_width::Tuple = (1e3,1e3), h_width::Tuple = (1e3,1e3))
-#     if isnothing(fluid)
-#         throw(error("Fluid not selected"))
-#     end
-#     p_crit = CoolProp.PropsSI("PCRIT",fluid)
-#     plot_sys = system[2:end-1];
-#     propx = :p
-#     propy = :h
-#     pp = [sol[getproperty(i.inport, propx)][1] for i in plot_sys]
-#     hh = [sol[getproperty(i.inport, propy)][1] for i in plot_sys]
-#     pmin = minimum(pp); pmax = maximum(pp)
-#     hmin = minimum(hh); hmax = maximum(hh)
-#     p_range = collect(range(pmin-p_width[1],pmax + p_width[2],points));
-#     h_range = collect(range(hmin-h_width[1],hmax + h_width[2],points));
-
-#     p_sat_curve = collect(range(pmin,p_crit,points))
-#     T_sat = PropsSI.("T","Q",1,"P",p_sat_curve,fluid)
-#     s_sat_liquid = PropsSI.("S","Q",0,"P",p_sat_curve,fluid)
-#     s_sat_gas = PropsSI.("S","Q",1,"P",p_sat_curve,fluid)
-#     h_sat_liquid = PropsSI.("H","Q",0,"P",p_sat_curve,fluid)
-#     h_sat_gas = PropsSI.("H","Q",1,"P",p_sat_curve,fluid)
-#     return p_range,h_range,T_sat,s_sat_liquid,s_sat_gas,h_sat_liquid,h_sat_gas,p_sat_curve
-# end
-
-
-# function PhasePlot(type::PhasePlotType_TS,sol::ODESolution,system::Vector{ODESystem},fluid::AbstractString=set_fluid)
-#     if isnothing(fluid)
-#         throw(error("Fluid not selected"))
-#     end
-#     plot_sys = system[2:end-1];
-#     propx = :p
-#     propy = :h
-#     pp = [sol[getproperty(i.inport, propx)][1] for i in plot_sys]
-#     hh = [sol[getproperty(i.inport, propy)][1] for i in plot_sys]
-
-#     p_range,h_range,T_sat,s_sat_liquid,s_sat_gas,h_sat_liquid,h_sat_gas,p_sat_curve = CollectPhaseData(sol,system,fluid)
-#     T_cycle_points = PropsSI.("T","P",pp,"H",hh,fluid) 
-#     s_cycle_points = PropsSI.("S","P",pp,"H",hh,fluid)
-#     T_data = []; s_data = []
-#     cycle_data_pp = [pp;pp[1]]; cycle_data_hh = [hh;hh[1]]
-#     for i = 1:size(pp,1)
-#         x1 = cycle_data_pp[i]; y1 = cycle_data_hh[i];
-#         x2 = cycle_data_pp[i+1]; y2 = cycle_data_hh[i+1];
-#         _,_,TT = LineData(T_line,x1,y1,x2,y2,fluid)
-#         T_data = append!(T_data,TT)
-#         _,_,ss = LineData(S_line,x1,y1,x2,y2,fluid)
-#         s_data = append!(s_data,ss)
-#     end
-#     fig1 = plot(s_data,T_data,label = "Cycle")
-#      scatter!(s_cycle_points,T_cycle_points,label="Cycle Points")
-#      plot!(s_sat_liquid,T_sat,label ="Saturation Curve liquid")
-#      plot!(s_sat_gas,T_sat,label ="Saturation Curve gas")
-#     xlabel!("Entropy (J/kg/K)")
-#     ylabel!("Temperature (K)")
-#     return fig1
-# end
-
-
-# # """
-# # `PhasePlot(type::PhasePlotType_PH,sol::ODESolution,system::Vector{ODESystem},fluid::AbstractString=set_fluid)`
-
-# # Plots the saturation curve of the fluid along with the cycle state-points. 
-# #  * Arguments:
-# #     1. `type`   : Chose between `PhasePlotType_PH` or `PhasePlotType_TS`
-# #     2. `sol`    : The solution from the ODE system. 
-# #     3. `system` : The vector of components chosen. The first should be `MassSource` and the last has to be `MassSink`.
-# # """
-# function PhasePlot(type::PhasePlotType_PH,sol::ODESolution,system::Vector{ODESystem},fluid::AbstractString=set_fluid)
-#     if isnothing(fluid)
-#         throw(error("Fluid not selected"))
-#     end
-#     plot_sys = system[2:end-1];
-#     propx = :p
-#     propy = :h
-#     pp = [sol[getproperty(i.inport, propx)][1] for i in plot_sys]
-#     hh = [sol[getproperty(i.inport, propy)][1] for i in plot_sys]
-#     p_range,h_range,T_sat,s_sat_liquid,s_sat_gas,h_sat_liquid,h_sat_gas,p_sat_curve = CollectPhaseData(sol,system,fluid)
-#     p_data = []; h_data = []
-#     cycle_data_pp = [pp;pp[1]]; cycle_data_hh = [hh;hh[1]]
-#     for i = 1:size(pp,1)
-#         x1 = cycle_data_pp[i]; y1 = cycle_data_hh[i];
-#         x2 = cycle_data_pp[i+1]; y2 = cycle_data_hh[i+1];
-#         h_ = collect(range(y1,y2,100)); h_data = append!(h_data,h_)
-#         p_ = collect(range(x1,x2,100)); p_data = append!(p_data,p_)
-#     end
-#     fig1 = plot(h_sat_liquid,p_sat_curve,label = "Saturation Curve liquid")
-#     plot!(h_sat_gas,p_sat_curve,label = "Saturation Curve gas")
-#     scatter!(hh,pp,label="Cycle Points")
-#     xlabel!("Enthalpy (J/kg)")
-#     ylabel!("Pressure (Pa)")
-#     plot!(h_data,p_data,label = "Cycle")
-#     return fig1
-# end
-
-# export PhasePlot
-
-# # """
-# # `CyclePlot(type::PhasePlotType_PH,sol::ODESolution,system::Vector{ODESystem},fluid::AbstractString=set_fluid)`
-
-# # Added Cycle ploting without saturation curve. Only plots the  cycle state points and their path on P-H plane. 
-
-# #  * Arguments:
-# #     1. `type`   : Chose between `PhasePlotType_PH` or `PhasePlotType_TS`
-# #     2. `sol`    : The solution from the ODE system. 
-# #     3. `system` : The vector of components chosen. The first should be `MassSource` and the last has to be `MassSink`.
-# #     4. `fluid`  : The fluid of chosen. Defaults to `set_fluid`
-# # """
-# function CyclePlot(type::PhasePlotType_PH,sol::ODESolution,system::Vector{ODESystem},fluid::AbstractString=set_fluid)
-#     if isnothing(fluid)
-#         throw(error("Fluid not selected"))
-#     end
-#     plot_sys = system[2:end-1];
-#     propx = :p
-#     propy = :h
-#     pp = [sol[getproperty(i.inport, propx)][1] for i in plot_sys]
-#     hh = [sol[getproperty(i.inport, propy)][1] for i in plot_sys]
-#     p_data = []; h_data = []
-#     cycle_data_pp = [pp;pp[1]]; cycle_data_hh = [hh;hh[1]]
-#     for i = 1:size(pp,1)
-#         x1 = cycle_data_pp[i]; y1 = cycle_data_hh[i];
-#         x2 = cycle_data_pp[i+1]; y2 = cycle_data_hh[i+1];
-#         h_ = collect(range(y1,y2,100)); h_data = append!(h_data,h_)
-#         p_ = collect(range(x1,x2,100)); p_data = append!(p_data,p_)
-#     end
-#     fig1 =  scatter(hh,pp,label="Cycle Points")
-#     plot!(h_data,p_data,label = "Cycle")
-#     xlabel!("Enthalpy (J/kg)")
-#     ylabel!("Pressure (Pa)")
-#     return fig1
-# end
-
-# # """
-# # `CyclePlot(type::PhasePlotType_TS,sol::ODESolution,system::Vector{ODESystem},fluid::AbstractString=set_fluid)`
-
-# # Added Cycle ploting without saturation curve. Only plots the  cycle state points and their path on T-S plane. 
-
-# #  * Arguments:
-# #     1. `type`   : Chose between `PhasePlotType_PH` or `PhasePlotType_TS`
-# #     2. `sol`    : The solution from the ODE system. 
-# #     3. `system` : The vector of components chosen. The first should be `MassSource` and the last has to be `MassSink`.
-# #     4. `fluid`  : The fluid of chosen. Defaults to `set_fluid`
-# # """
-# function CyclePlot(type::PhasePlotType_TS,sol::ODESolution,system::Vector{ODESystem},fluid::AbstractString=set_fluid)
-#     if isnothing(fluid)
-#         throw(error("Fluid not selected"))
-#     end
-#     plot_sys = system[2:end-1];
-#     propx = :p
-#     propy = :h
-#     pp = [sol[getproperty(i.inport, propx)][1] for i in plot_sys]
-#     hh = [sol[getproperty(i.inport, propy)][1] for i in plot_sys]
-#     T_cycle_points = PropsSI.("T","P",pp,"H",hh,fluid) 
-#     s_cycle_points = PropsSI.("S","P",pp,"H",hh,fluid)
-#     T_data = []; s_data = []
-#     cycle_data_pp = [pp;pp[1]]; cycle_data_hh = [hh;hh[1]]
-#     for i = 1:size(pp,1)
-#         x1 = cycle_data_pp[i]; y1 = cycle_data_hh[i];
-#         x2 = cycle_data_pp[i+1]; y2 = cycle_data_hh[i+1];
-#         _,_,TT = LineData(T_line,x1,y1,x2,y2,fluid)
-#         T_data = append!(T_data,TT)
-#         _,_,ss = LineData(S_line,x1,y1,x2,y2,fluid)
-#         s_data = append!(s_data,ss)
-#     end
-#     fig1 = plot(s_data,T_data,label = "Cycle")
-#      scatter!(s_cycle_points,T_cycle_points,label="Cycle Points")
-#      xlabel!("Entropy (J/kg/K)")
-#      ylabel!("Temperature (K)")
-#      return fig1
-# end
-
-# export CyclePlot
-
-
-struct PhasePlot_parameters
-    type::Symbol
-    p_range::Tuple
-    h_range::Tuple
-    N::Int64
-    z::Any
-    function PhasePlot_parameters(;type::Symbol,p_range::Tuple,h_range::Tuple,N::Int64,z::Any)
-        @assert p_range[1] <= p_range[2]
-        @assert h_range[1] <= h_range[2]
-        new(type,p_range,h_range,N,z)
+function PhasePlot_TS(fig,fluid::EoSModel,p,z;N=100)
+    p_min = minimum(p)
+    p_max = CarnotCycles.CriticalPressure(fluid,z) 
+    p_ = collect(range(p_min,p_max,N))
+    T_bubble = zeros(N)
+    T_dew = zeros(N)
+    s_bubble = zeros(N)
+    s_dew = zeros(N)
+    for i in 1:N
+        T_bubble[i] = CarnotCycles.qp_temperature(fluid,0.0,p_[i],z)
+        s_bubble[i] = CarnotCycles.qp_entropy(fluid,0.0,p_[i],z)
+        T_dew[i] = CarnotCycles.qp_temperature(fluid,1.0,p_[i],z)
+        s_dew[i] = CarnotCycles.qp_entropy(fluid,1.0,p_[i],z)
     end
+    plot!(fig,s_bubble,T_bubble,label = "Bubble Point",xlabel = "Entropy (J/K)", ylabel = "Temperature (K)",alpha = 0.3,linewidth=5)
+    plot!(fig,s_dew,T_dew,label = "Dew Point",xlabel = "Entropy (J/K)", ylabel = "Temperature (K)",alpha = 0.3,linewidth=5)
 end
 
-function PhasePlot(plot_param::PhasePlot_parameters;fluid=set_fluid)
-    if isnothing(fluid)
-        throw(error("Fluid not selected"))
+function PhasePlot_TS(fig,fluid::AbstractString,p;N=100)
+    p_min = minimum(p)
+    p_max = CoolProp.PropsSI("PCRIT",fluid)
+    p_ = collect(range(p_min,p_max,N))
+    T_bubble = zeros(N)
+    T_dew = zeros(N)
+    s_bubble = zeros(N)
+    s_dew = zeros(N)
+    for i in 1:N
+        T_bubble[i] = CarnotCycles.PropsSI("T","P",p_[i],"Q",0.0,fluid)
+        s_bubble[i] = CarnotCycles.PropsSI("S","P",p_[i],"Q",0.0,fluid)
+        T_dew[i] = CarnotCycles.PropsSI("T","P",p_[i],"Q",1.0,fluid)
+        s_dew[i] = CarnotCycles.PropsSI("S","P",p_[i],"Q",1.0,fluid)
     end
-    @unpack type,p_range,h_range,N,z = plot_param
-
-    if type != :PH || type != :TS
-        throw(error("The type selected for phase plots is not :TS or :PH"))
-    end
-
+    plot!(fig,s_bubble,T_bubble,label = "Bubble Point",xlabel = "Entropy (J/K)", ylabel = "Temperature (K)",linewidth=5,alpha = 0.3)
+    plot!(fig,s_dew,T_dew,label = "Dew Point",xlabel = "Entropy (J/K)", ylabel = "Temperature (K)",linewidth=5,alpha = 0.3)
 end
 
-
-function PhasePlotClapeyron(plot_param::PhasePlot_parameters;fluid=set_fluid)
-    @assert fluid isa EoSModel
-    @unpack type,p_range,h_range,N,z = plot_param
-    @assert z isa AbstractVector
-    p = collect(range(p_range[1],p_range[2],N)); h = collect(range(h_range[1],h_range[2],N))
-
-    mix_phase_identification(fluid,p,h) = PhaseIdentification(fluid,p,h,z)
-
-    phase = zeros(N,N)
-
-    for i in eachindex(phase,1)
-        for j in eachindex(phase,2)
-            try
-                phase[i,j] = mix_phase_identification(fluid,p[i],h[j])
-            catch
-                phase[i,j] = NaN
-            end
-        end
+function PhasePlot_PH(fig,fluid::EoSModel,p,z,N=100)
+    p_min = minimum(p)
+    p_max = CarnotCycles.CriticalPressure(fluid,z) 
+    p_ = collect(range(p_min,p_max,N))
+    h_bubble = zeros(N)
+    h_dew = zeros(N)
+    for i in 1:N
+        h_bubble[i] = CarnotCycles.qp_enthalpy(fluid,0.0,p_[i],z)
+        h_dew[i] = CarnotCycles.qp_enthalpy(fluid,1.0,p_[i],z)
     end
-    return phase
-end
-
-function PhasePlotCoolProp()
+    plot!(fig,h_bubble,p_,label = "Bubble Point",ylabel = "Pressure (Pa)", xlabel = "Enthalpy (J)",alpha = 0.3,linewidth=5)
+    plot!(fig,h_dew,p_,label = "Dew Point",ylabel = "Pressure (Pa)", xlabel = "Enthalpy (J)",alpha = 0.3,linewidth=5)
     
 end
+
+function PhasePlot_PH(fig,fluid::AbstractString,p,N=100)
+    p_min = minimum(p)
+    p_max = CoolProp.PropsSI("PCRIT",fluid)
+    p_ = collect(range(p_min,p_max,N))
+    h_bubble = zeros(N)
+    h_dew = zeros(N)
+    for i in 1:N
+        h_bubble[i] = CarnotCycles.PropsSI("H","P",p_[i],"Q",0.0,fluid)
+        h_dew[i] = CarnotCycles.PropsSI("H","P",p_[i],"Q",1.0,fluid)
+    end
+    plot!(fig,h_bubble,p_,label = "Bubble Point",xlabel = "Enthalpy (J/K/kg)", ylabel = "Pressure (Pa)",linewidth=5,alpha = 0.3)
+    plot!(fig,h_dew,p_,label = "Dew Point",xlabel = "Enthalpy (J/K/kg)", ylabel = "Pressure (Pa)",linewidth=5,alpha = 0.3)
+    
+end
+
+function plot_PH(sol::SteadyStateSolution,system::Vector{ODESystem},names::Vector{String};phase = true,fluid = CarnotCycles.set_fluid)
+    if fluid isa EoSModel
+        @assert length(system)-2 == length(names) "Length of system and names must be equal, excluding source and sink"
+        system_ = system[2:end-1] # Exclude source and sink
+        z = sol[system_[1].z_in]
+        h = zeros(length(system_))
+        p = zeros(length(system_))
+        for i in eachindex(names)
+            h[i] = sol[system_[i].h_in]
+            p[i] = sol[system_[i].p_in]
+        end    
+        append!(h,sol[system[end].h])
+        append!(p,sol[system[end].p])
+        fig = Plots.plot()
+    
+        if phase == true
+            PhasePlot_PH(fig,fluid,p,z)
+        end
+        for i = 1: length(names)
+            h_ = collect(range(h[i],h[i+1],100))
+            p_ = collect(range(p[i],p[i+1],100))
+            fig = plot!(fig,h_,p_,label = names[i],xlabel = "Enthalpy (J/K)", ylabel = "Pressure (Pa)", title = "Pressure-Enthalpy Diagram")
+        end
+        return fig
+    end
+
+    if fluid isa AbstractString
+        @assert length(system)-2 == length(names) "Length of system and names must be equal, excluding source and sink"
+        system_ = system[2:end-1] # Exclude source and sink
+        h = zeros(length(system_))
+        p = zeros(length(system_))
+        for i in eachindex(names)
+            h[i] = sol[system_[i].h_in]
+            p[i] = sol[system_[i].p_in]
+        end    
+        append!(h,sol[system[end].h])
+        append!(p,sol[system[end].p])
+        fig = Plots.plot()
+    
+        if phase == true
+            PhasePlot_PH(fig,fluid,p)
+        end
+        for i = 1: length(names)
+            h_ = collect(range(h[i],h[i+1],100))
+            p_ = collect(range(p[i],p[i+1],100))
+            fig = plot!(fig,h_,p_,label = names[i],xlabel = "Enthalpy (J/K/kg)", ylabel = "Pressure (Pa)", title = "Pressure-Enthalpy Diagram")
+        end
+        return fig
+    end
+end
+
+function plot_TS(sol::SteadyStateSolution,system::Vector{ODESystem},names::Vector{String};phase = true,fluid = CarnotCycles.set_fluid)
+    if fluid isa EoSModel
+        @assert length(system)-2 == length(names) "Length of system and names must be equal, excluding source and sink"
+        system_ = system[2:end-1] # Exclude source and sink
+        z = sol[system_[1].z_in]
+        h = zeros(length(system_))
+        p = zeros(length(system_))
+        for i in eachindex(names)
+            h[i] = sol[system_[i].h_in]
+            p[i] = sol[system_[i].p_in]
+        end    
+        append!(h,sol[system[end].h])
+        append!(p,sol[system[end].p])
+        fig = Plots.plot()
+    
+        if phase == true
+            PhasePlot_TS(fig,fluid,p,z)
+        end
+        for i = 1: length(names)
+            h_ = collect(range(h[i],h[i+1],100))
+            p_ = collect(range(p[i],p[i+1],100))
+            T_ = similar(h_)
+            s_ = similar(h_)
+            for j in eachindex(h_)
+                T_[j] = CarnotCycles.ph_temperature_qp(fluid,p_[j],h_[j],CarnotCycles.mass_to_moles(CarnotCycles.set_fluid,sol[system_[i].inport.x],sol[system_[i].inport.mdot]))
+                s_[j] = CarnotCycles.ph_entropy_qp(fluid,p_[j],h_[j],CarnotCycles.mass_to_moles(CarnotCycles.set_fluid,sol[system_[i].inport.x],sol[system_[i].inport.mdot]))
+            end
+            fig = plot!(fig,s_,T_,label = names[i],xlabel = "Entropy (J/K)", ylabel = "Temperature (K)", title = "Temperature-Entropy Diagram")
+        end
+        return fig
+    end
+
+    if fluid isa AbstractString
+        @assert length(system)-2 == length(names) "Length of system and names must be equal, excluding source and sink"
+        system_ = system[2:end-1] # Exclude source and sink
+        h = zeros(length(system_))
+        p = zeros(length(system_))
+        for i in eachindex(names)
+            h[i] = sol[system_[i].h_in]
+            p[i] = sol[system_[i].p_in]
+        end    
+        append!(h,sol[system[end].h])
+        append!(p,sol[system[end].p])
+        fig = Plots.plot()
+    
+        if phase == true
+            PhasePlot_TS(fig,fluid,p)
+        end
+        for i = 1: length(names)
+            h_ = collect(range(h[i],h[i+1],100))
+            p_ = collect(range(p[i],p[i+1],100))
+            T_ = similar(h_)
+            s_ = similar(h_)
+            for j in eachindex(h_)
+                T_[j] = CarnotCycles.PropsSI("T","P",p_[j],"H",h_[j],fluid)
+                s_[j] = CarnotCycles.PropsSI("S","P",p_[j],"H",h_[j],fluid)
+            end
+            fig = plot!(fig,s_,T_,label = names[i],xlabel = "Entropy (J/K/kg)", ylabel = "Temperature (K)", title = "Temperature-Entropy Diagram")
+        end
+        return fig
+    end
+end
+
+"""
+`plot(sol::SteadyStateSolution,system::Vector{ODESystem},names::Vector{String};phase = true,fluid = CarnotCycles.set_fluid,type = :TS)`
+    * Plots the phase diagram of the system using the given solution and system.
+    * `sol`: The solution object containing the results of the simulation. Do not include source and sink in the system.
+    * `system`: The vector of ODESystem objects representing the system.
+    * `names`: The vector of names for each component in the system.
+    * `phase`: A boolean indicating whether to plot the phase boundaries or not. Default is true.
+    * `fluid`: The fluid model to be used for plotting. Default is `CarnotCycles.set_fluid`.
+    * `type`: The type of plot to be generated. Can be either :TS or :PH. Default is :TS.
+
+    Returns a `Plots.plot` object.
+"""
+function plot(sol::SteadyStateSolution,system::Vector{ODESystem},names::Vector{String};phase = true,fluid = CarnotCycles.set_fluid,type = :TS)
+    if type == :TS
+        return plot_TS(sol,system,names;phase = phase,fluid = fluid)
+    elseif type == :PH
+        return plot_PH(sol,system,names;phase = phase,fluid = fluid)
+    else
+        error("Type must be either :TS or :PH")
+    end
+    
+end
+
+export PhasePlot_TS, PhasePlot_PH, plot_TS, plot_PH, plot
