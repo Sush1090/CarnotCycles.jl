0de266a6f8f453d92685dc1ed4c6f41f57bb6c2c
Sush1090 <sushrut1090@gmail.com>
2025-06-17 11:37:41 +0200

    changed sol to `SteadStateSolution`

--- src/ThemodynamicClaculations.jl
+++ src/ThemodynamicClaculations.jl
@@ -25,7 +25,7 @@ end
 """
 `Compute_cycle_error(sol::ODESolution,system::Vector{System};reltol = 1e-8)`
 """
-function Compute_cycle_error(sol::ODESolution,system::Vector{System};reltol = 1e-8)
+function Compute_cycle_error(sol::SteadyStateSolution,system::Vector{System};reltol = 1e-8)
     source = system[1]; sink = system[end]
     h_source = sol[source.h][1]; p_source = sol[sink.p][1];
     h_sink = sol[sink.h][1]; p_sink = sol[sink.p][1];
